3b29d099532a8c410eb2ecc51a4bfa3e247777e2
masahito1997 <velonica.gale2227@gmail.com>
2021-03-21 18:41:03 +0900

    set basic assets configurations

--- app/javascript/packs/blog.js
+++ app/javascript/packs/blog.js
@@ -3,15 +3,9 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import "../src/stylesheets/application.scss"
+import "../src/stylesheets/blog.scss"
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
-
-import "jquery/dist/jquery"
-import "popper.js/dist/popper"
-import "bootstrap/dist/js/bootstrap"
