f2e9ccc0a8601c5aaa91aca0ecc5e6375ddcab2f
masahito1997 <velonica.gale2227@gmail.com>
2021-03-23 07:53:46 +0900

    implement showing saved tags in tag form

--- app/javascript/packs/tag.js
+++ app/javascript/packs/tag.js
@@ -3,8 +3,8 @@ import Taggle from 'taggle'
 $(document).ready(() => {
   if ($('#article-tag-form').length) {
     new Taggle('article-tag-form', {
-      tags: [],
-      hiddenInputName: 'article[tags_attributes][name][]'
+      tags: tag_names,
+      hiddenInputName: 'article[tags][]'
     })
   }
 });
