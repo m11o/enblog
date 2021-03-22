8a7fac7e6c3934c63711821a1451566fc5c2215a
masahito1997 <velonica.gale2227@gmail.com>
2021-03-23 07:42:12 +0900

    remove turbolinks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,11 +5,9 @@
 
 import "../src/stylesheets/application.scss"
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
 
 import "jquery/dist/jquery"
