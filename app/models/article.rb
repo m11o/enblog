2148176497e67725374eef568263a88a3d446306
masahito1997 <velonica.gale2227@gmail.com>
2021-03-22 15:37:09 +0900

    add code colum in article

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,11 +3,12 @@
 # Table name: articles
 #
 #  id           :bigint           not null, primary key
+#  code         :string(255)      not null
 #  title        :string(255)      not null
 #  description  :text(65535)
 #  body         :text(16777215)   not null
-#  language     :integer          default(0), not null
-#  state        :integer          default(0), not null
+#  language     :integer          default("japanese"), not null
+#  state        :integer          default("closed"), not null
 #  published_at :datetime
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
