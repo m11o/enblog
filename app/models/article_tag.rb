f03d0ddc5c1ee86b23bae38fbc12b4827bacd1fe
masahito1997 <velonica.gale2227@gmail.com>
2021-03-22 14:54:13 +0900

    add article_tag model

---
+++ app/models/article_tag.rb
@@ -0,0 +1,12 @@
+# == Schema Information
+#
+# Table name: article_tags
+#
+#  id         :bigint           not null, primary key
+#  article_id :integer          not null
+#  tag_id     :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class ArticleTag < ApplicationRecord
+end
