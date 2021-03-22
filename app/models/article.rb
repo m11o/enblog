4ff01fe204b44a924958fbb2980a331ad0690091
masahito1997 <velonica.gale2227@gmail.com>
2021-03-22 15:37:39 +0900

    add code validation in article

--- app/models/article.rb
+++ app/models/article.rb
@@ -17,6 +17,7 @@ class Article < ApplicationRecord
   has_many :article_tags, dependent: :destroy
   has_many :tags, through: :article_tags
 
+  validates :code, presence: true
   validates :title, presence: true
   validates :body, presence: true
   validates :language, presence: true
