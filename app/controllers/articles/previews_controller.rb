53f4a74f0cf095e16ab8d453e3ba65d443e92750
masahito1997 <velonica.gale2227@gmail.com>
2021-03-23 10:34:53 +0900

    fix rubocop

--- app/controllers/articles/previews_controller.rb
+++ app/controllers/articles/previews_controller.rb
@@ -1,24 +1,26 @@
-class Articles::PreviewsController < ApplicationController
-  layout 'blog'
+module Articles
+  class PreviewsController < ApplicationController
+    layout 'blog'
 
-  before_action :set_preview
+    before_action :set_preview
 
-  def index
-    @articles = Article.includes(:tags).order(published_at: :desc, id: :desc).limit(25)
+    def index
+      @articles = Article.includes(:tags).order(published_at: :desc, id: :desc).limit(25)
 
-    render 'blog/index'
-  end
+      render 'blog/index'
+    end
 
-  def show
-    @article = Article.find_by code: params[:code]
-    @related_articles = @article.recommend_articles
+    def show
+      @article = Article.find_by code: params[:code]
+      @related_articles = @article.recommend_articles
 
-    render 'blog/show'
-  end
+      render 'blog/show'
+    end
 
-  private
+    private
 
-  def set_preview
-    @preview = true
+    def set_preview
+      @preview = true
+    end
   end
 end
