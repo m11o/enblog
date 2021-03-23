0bea860b058049baf486a7d84482c53134374383
masahito1997 <velonica.gale2227@gmail.com>
2021-03-23 10:31:38 +0900

    refactor routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root to: 'articles#index'
 
-  resources :articles
   namespace :articles do
     resources :previews, only: %i[index show], param: :code
   end
+  resources :articles
 end
