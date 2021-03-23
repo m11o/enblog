7ea731e79ac79e0c5ad4120684dd209524bcf221
masahito1997 <velonica.gale2227@gmail.com>
2021-03-23 10:32:22 +0900

    implement preview controller

---
+++ app/controllers/articles/previews_controller.rb
@@ -0,0 +1,24 @@
+class Articles::PreviewsController < ApplicationController
+  layout 'blog'
+
+  before_action :set_preview
+
+  def index
+    @articles = Article.includes(:tags).order(published_at: :desc, id: :desc).limit(25)
+
+    render 'blog/index'
+  end
+
+  def show
+    @article = Article.find_by code: params[:code]
+    @related_articles = @article.recommend_articles
+
+    render 'blog/show'
+  end
+
+  private
+
+  def set_preview
+    @preview = true
+  end
+end
