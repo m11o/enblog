a0bbf15a7d9381dc7ea4776e26fc37e0df9d5b52
masahito1997 <velonica.gale2227@gmail.com>
2021-03-24 11:22:56 +0900

    fix rubocop

--- app/services/aws/purge_cache_service.rb
+++ app/services/aws/purge_cache_service.rb
@@ -7,11 +7,11 @@ module Aws
         return if paths.blank?
 
         response = create_invalidation! paths
-        if response.blank? or response.invalidation.blank? or response.invalidation.status != 'InProgress'
-          Rails.logger.warn "Fail to delete cache of #{paths.join(', ')}"
-          Rails.logger.warn "Response from CloudFront: #{response.inspect}"
-          raise 'キャッシュの削除に失敗しました。'
-        end
+        return if response.present? && response.invalidation.present? && (response.invalidation.status == 'InProgress')
+
+        Rails.logger.warn "Fail to delete cache of #{paths.join(', ')}"
+        Rails.logger.warn "Response from CloudFront: #{response.inspect}"
+        raise 'キャッシュの削除に失敗しました。'
       end
 
       private
@@ -22,7 +22,6 @@ module Aws
             distribution_id: ENV['AWS_CLOUD_FRONT_DISTRIBUTION_ID'],
             invalidation_batch: {
               paths: { quantity: paths.size, items: paths },
-              # api callを同定するためのユニークな文字列
               # @see http://docs.aws.amazon.com/cloudfront/latest/APIReference/API_CreateInvalidation.html#API_CreateInvalidation_RequestSyntax
               caller_reference: Time.now.to_s
             }
