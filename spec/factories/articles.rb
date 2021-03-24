61049d488f894c7f99c67e2ac4900af30f458b83
masahito1997 <velonica.gale2227@gmail.com>
2021-03-24 14:31:17 +0900

    add annotate

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id           :bigint           not null, primary key
+#  code         :string(255)      not null
+#  title        :string(255)      not null
+#  description  :text(65535)
+#  language     :integer          default("japanese"), not null
+#  state        :integer          default("closed"), not null
+#  published_at :datetime
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+# Indexes
+#
+#  index_articles_on_code  (code) UNIQUE
+#
 FactoryBot.define do
   factory :article do
     title { Faker::App.name }
