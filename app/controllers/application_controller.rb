1dfe8a1231fef43ba8cad1313656d6df249fbf9c
masahito1997 <velonica.gale2227@gmail.com>
2021-03-25 14:49:45 +0900

    implement basic auth method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :require_basic_auth
+
+  private
+
+  def require_basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == Rails.application.credentials.basic_auth[:username] &&
+        password == Rails.application.credentials.basic_auth[:password]
+    end
+  end
 end
